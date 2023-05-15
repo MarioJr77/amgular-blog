a70503ea55dc849ab462ff14a3b5ec8b3c575f6f
Mario Junior <mlgj77@yahoo.com.br>
2023-05-15 19:20:26 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover": "https://www.nme.com/wp-content/uploads/2019/05/avengersrdj.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Lorem ipsum lorem ipsum",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2022/08/Marvel-Studios-MCU.jpg"
+  }
+]
